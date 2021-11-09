268732fdc5d92666716bbb37898e8628ae6db5e6
BrianStritch <brians12ca18@gmail.com>
2021-11-09 21:14:51 +0000

    Add function to create a game board for battleship game

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+
+def player_game_board():
+    """
+    Function to create game board 10 units by 10 units.
+    """
+    game_board = []
+    for rowcol in range(0, 10):
+        game_board.append([' . '] * 10)
+    print()
+    for row in game_board:
+        print(''.join(row))
+    print()
+
+
+player_game_board()
