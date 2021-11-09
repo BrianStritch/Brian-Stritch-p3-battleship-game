b986d81276292b7bab9777a4937bf7dd384df668
BrianStritch <brians12ca18@gmail.com>
2021-11-09 21:47:54 +0000

    add function to create game board for computer, add function to obtain username and issue welcome msg

--- run.py
+++ run.py
@@ -1,9 +1,22 @@
 from random import randint
 
 
-def player_game_board():
+def welcome():
     """
-    Function to create game board 10 units by 10 units.
+    get Username from the user
+    """
+    print()
+    print('------------------------------')
+    print("Welcome to Brian's battleship game.")
+    print('------------------------------')
+    user = input('Enter your name here:\n')
+    print(f"Welcome {user}, good luck, you'll need it")
+    return user
+
+
+def player_game_board(user):
+    """
+    Function to create game board 10 units by 10 units for player.
     """
     game_board = []
     for rowcol in range(0, 10):
@@ -11,7 +24,26 @@ def player_game_board():
     print()
     for row in game_board:
         print(''.join(row))
+    print(f"{user}'s game board")
+
+
+def computer_game_board():
+    """
+    Function to create game board 10 units by 10 units for computer.
+    """
+    computer_game_board = []
+    for rowcol in range(0, 10):
+        computer_game_board.append([' . '] * 10)
     print()
+    for row in computer_game_board:
+        print(''.join(row))
+    print("Computer's game board")
 
 
-player_game_board()
+def main():
+    username = welcome()    
+    player_game_board(username)
+    computer_game_board()
+
+
+main()
